ec7bb81fdd05f6bf9be186df8edad47ca6a225ea
evandro dos santos dias <evandro.dias@cicc.local>
2025-06-09 15:47:07 -0300

    trabalho-portifilo

---
+++ Front-End-essencial/trabalho-portifolio/script.js
@@ -0,0 +1,15 @@
+const inputNome = document.getElementById('nome')
+const inputEmail = document.getElementById('email')
+const btnEnviar = document.getElementById('btnEnviar')
+
+btnEnviar.addEventListener("click", ()=>{
+    if(!inputNome.value || !inputEmail.value){
+        alert(`Preencha todos os campos do formulário`)
+    }else{
+        alert(`Menssagem enviada com sucesso! Agradeço pelo seu contato.`)
+    }
+})
+
+document.querySelector("#linkedin").addEventListener('click', ()=>{
+    alert(`Desculpe perfil no Linkedin em manutenção!!`)
+})
