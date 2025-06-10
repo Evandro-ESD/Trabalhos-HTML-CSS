4c3cbe68d9a84f639a3f3d439e8741cfc1191765
Evandro-ESD <vando86206@gmail.com>
2025-06-10 15:48:50 -0300

    Delete Aula 04 directory

--- Aula 04/aula/array.js
+++
@@ -1,8 +0,0 @@
-let frutas = ["maçã", "banana", "laranja"]
-
-
-for(let i = 0; i < frutas.length; i++){
-// for(let i = 0; i < 5; i++){
-
-    console.log(`${i + 1}ª fruta da lista na posição de índex ${i}: ${frutas[i]}`)
-}
